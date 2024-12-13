4c969f11fa7663bacb324b89306d7a98fde84e90
Panczel24 <pcsenge8@gmail.com>
2024-12-13 11:36:45 +0100

    F02 feladat elkészítve

--- orai/Program.cs
+++ orai/Program.cs
@@ -11,31 +11,51 @@ namespace orai
 		static Random r = new Random();
 		static void Main(string[] args)
 		{
+
+
 			f01();
+
+
+            int[] szamok = {10,15,24,22,21,32,61,87 }; // lesz egy referenciacime a memóriában: hol van a tömböm
+			Console.WriteLine();
+			Console.WriteLine("MÁSODIK FELADAT");
+			Console.WriteLine("ennyi páros szám volt:"+ f02(szamok)); //csak a cimet adom át
 
 		}
 
 		static void f01()
 		{
 			int[] szamok = new int[15];
-            Console.WriteLine("eredeti szám:");
-            for (int i = 0; i < szamok.Length; i++)
+			Console.WriteLine("eredeti szám:");
+			for (int i = 0; i < szamok.Length; i++)
 			{
 				szamok[i] = r.Next(1, 50); ;
 				Console.Write(szamok[i] + " , ");
-
 			}
             Console.WriteLine();
-            Console.WriteLine("módosított szám:");
-			for (int i = 0; i < szamok.Length; i++)
+            Console.WriteLine("Módosított szám");
+            for (int i = 0; i < szamok.Length; i++)
 			{
 				int random = r.Next(1, 50);
 				szamok[i] += random;
 				Console.Write(szamok[i] +" , ");
 			}
+            Console.WriteLine();
         }
 
+		static int f02(int[] tomb)
+		{
+			int szam = 0;
+			for (int i = 0; i < tomb.Length; i++)
+			{
+				if (tomb[i] %2 == 0)
+				{
+					szam++;
+				}
+			}
+			return szam;
 
+		}
 
 
 

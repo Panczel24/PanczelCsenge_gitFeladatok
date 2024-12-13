5612c3d0fa792d175e6dc0a195707bee4c8cf778
Panczel24 <pcsenge8@gmail.com>
2024-12-13 10:40:39 +0100

    F01 feladat elkészítve

--- orai/Program.cs
+++ orai/Program.cs
@@ -8,8 +8,43 @@ namespace orai
 {
 	internal class Program
 	{
+		static Random r = new Random();
 		static void Main(string[] args)
 		{
+			f01();
+
 		}
+
+		static void f01()
+		{
+			int[] szamok = new int[15];
+            Console.WriteLine("eredeti szám:");
+            for (int i = 0; i < szamok.Length; i++)
+			{
+				szamok[i] = r.Next(1, 50); ;
+				Console.Write(szamok[i] + " , ");
+
+			}
+            Console.WriteLine();
+            Console.WriteLine("módosított szám:");
+			for (int i = 0; i < szamok.Length; i++)
+			{
+				int random = r.Next(1, 50);
+				szamok[i] += random;
+				Console.Write(szamok[i] +" , ");
+			}
+        }
+
+
+
+
+
+
+
+
+
+
+
+
 	}
 }
